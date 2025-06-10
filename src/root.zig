154d94cee6bdf739efb8cb0f0152b4005da9f928
oilsoundsfunny <anbinhthuongnhatnam@gmail.com>
2025-06-10 23:42:14 +0700

    update root.zig

--- src/root.zig
+++ src/root.zig
@@ -4,10 +4,21 @@ const engine = @import("engine");
 const misc = @import("misc");
 const std = @import("std");
 
+pub const std_options = std.Options {
+	.log_level = .info,
+	.side_channels_mitigations = .basic,
+};
+
 pub fn main() !void {
 	try engine.Thread.Pool.global.allocate(1);
+	try engine.Thread.Pool.global.workers.?[0].pos.parseFen(
+	  \\rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR
+	  \\ w KQkq - 0 1
+	);
+
 	try engine.transposition.Table.global.allocate(64);
 	try engine.uci.printEngine();
+
 	try misc.time.init();
 	defer misc.heap.arena.deinit();
 
