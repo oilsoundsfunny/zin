b8a8a000bff1feb70586bb9b4eab4195d66bce5b
oilsoundsfunny <anbinhthuongnhatnam@gmail.com>
2026-02-25 11:14:57 +0700

    same same but different
    bench: 5700149

--- src/nnue/Accumulator.zig
+++ src/nnue/Accumulator.zig
@@ -17,6 +17,50 @@ pub const Vec = @Vector(network.Default.l0s, i16);
 pub const Feature = struct {
     piece: types.Piece,
     square: types.Square,
+
+    pub fn init(p: types.Piece, s: types.Square) Feature {
+        return .{
+            .piece = p,
+            .square = s,
+        };
+    }
+
+    pub fn transform(self: Feature, target: anytype) switch (@TypeOf(target)) {
+        types.Square, types.Square.Set, Feature => |T| T,
+        else => @compileError("unexpected type"),
+    } {
+        const pt = self.piece.ptype();
+        const pc = self.piece.color();
+        const hm = switch (self.square.file()) {
+            .file_a, .file_b, .file_c, .file_d => false,
+            else => true,
+        };
+
+        std.debug.assert(pt == .king);
+        return if (@TypeOf(target) == Feature) blk: {
+            const target_pt = target.piece.ptype();
+            const target_pc = target.piece.color();
+            break :blk .{
+                .piece = .init(target_pt, if (target_pc == pc) .white else .black),
+                .square = self.transform(target.square),
+            };
+        } else blk: {
+            const ret = if (pc == .black) target.flipRank() else target;
+            break :blk if (hm) ret.flipFile() else ret;
+        };
+    }
+
+    pub fn bucket(self: Feature) usize {
+        std.debug.assert(self.piece.ptype() == .king);
+        return network.Default.buckets.get(self.transform(self.square));
+    }
+
+    pub fn index(self: Feature) usize {
+        const ci: usize = if (self.piece.color() == .white) 0 else types.Ptype.num;
+        const pi: usize = self.piece.ptype().int();
+        const si: usize = self.square.int();
+        return (ci + pi) * types.Square.num + si;
+    }
 };
 
 pub const Perspective = struct {
--- src/nnue/network.zig
+++ src/nnue/network.zig
@@ -15,14 +15,6 @@ const page_size = std.heap.pageSize();
 pub const Default = Network(.{
     .input_buckets = .init(.{
         // zig fmt: off
-        // .a8 = 3, .b8 = 3, .c8 = 3, .d8 = 3, .e8 = 3, .f8 = 3, .g8 = 3, .h8 = 3,
-        // .a7 = 3, .b7 = 3, .c7 = 3, .d7 = 3, .e7 = 3, .f7 = 3, .g7 = 3, .h7 = 3,
-        // .a6 = 3, .b6 = 3, .c6 = 3, .d6 = 3, .e6 = 3, .f6 = 3, .g6 = 3, .h6 = 3,
-        // .a5 = 3, .b5 = 3, .c5 = 3, .d5 = 3, .e5 = 3, .f5 = 3, .g5 = 3, .h5 = 3,
-        // .a4 = 2, .b4 = 2, .c4 = 2, .d4 = 2, .e4 = 2, .f4 = 2, .g4 = 2, .h4 = 2,
-        // .a3 = 2, .b3 = 2, .c3 = 2, .d3 = 2, .e3 = 2, .f3 = 2, .g3 = 2, .h3 = 2,
-        // .a2 = 0, .b2 = 0, .c2 = 1, .d2 = 1, .e2 = 1, .f2 = 1, .g2 = 0, .h2 = 0,
-        // .a1 = 0, .b1 = 0, .c1 = 1, .d1 = 1, .e1 = 1, .f1 = 1, .g1 = 0, .h1 = 0,
         .a8 = 0, .b8 = 0, .c8 = 0, .d8 = 0, .e8 = 0, .f8 = 0, .g8 = 0, .h8 = 0,
         .a7 = 0, .b7 = 0, .c7 = 0, .d7 = 0, .e7 = 0, .f7 = 0, .g7 = 0, .h7 = 0,
         .a6 = 0, .b6 = 0, .c6 = 0, .d6 = 0, .e6 = 0, .f6 = 0, .g6 = 0, .h6 = 0,
@@ -84,8 +76,9 @@ pub fn Network(comptime opts: Options) type {
 
         const Self = @This();
 
+        pub const buckets = opts.input_buckets;
         pub const inp = types.Piece.num * types.Square.num;
-        pub const ibn = std.mem.max(usize, opts.input_buckets.values[0..]) + 1;
+        pub const ibn = std.mem.max(usize, buckets.values[0..]) + 1;
         pub const l0s = if (opts.hl_size % 32 == 0)
             opts.hl_size
         else
@@ -99,43 +92,6 @@ pub fn Network(comptime opts: Options) type {
         pub const qb = 64;
         pub const scale = 360;
 
-        pub fn buckets(
-            kings: std.EnumArray(types.Color, types.Square),
-        ) std.EnumArray(types.Color, usize) {
-            var a: std.EnumArray(types.Color, usize) = .initFill(ibn);
-            for (types.Color.values) |c| {
-                const s = kings.get(c);
-                const pov = if (c == .black) s.flipRank() else s;
-                const hmd = switch (pov.file()) {
-                    .file_a, .file_b, .file_c, .file_d => pov,
-                    else => pov.flipFile(),
-                };
-                a.set(c, opts.input_buckets.get(hmd));
-            }
-            return a;
-        }
-
-        pub fn indices(
-            kings: std.EnumArray(types.Color, types.Square),
-            ft: Accumulator.Feature,
-        ) std.EnumArray(types.Color, usize) {
-            var a: std.EnumArray(types.Color, usize) = .initFill(inp);
-            for (types.Color.values) |c| {
-                const s = ft.square;
-                const pov = if (c == .black) s.flipRank() else s;
-                const hmd = switch (kings.get(c).file()) {
-                    .file_a, .file_b, .file_c, .file_d => pov,
-                    else => pov.flipFile(),
-                };
-
-                const ci: usize = if (ft.piece.color() == c) 0 else types.Ptype.num;
-                const pi: usize = ft.piece.ptype().int();
-                const si: usize = hmd.int();
-                a.set(c, (ci + pi) * types.Square.num + si);
-            }
-            return a;
-        }
-
         pub fn infer(
             self: *const Self,
             perspective: *const Accumulator.Perspective,
@@ -148,11 +104,11 @@ pub fn Network(comptime opts: Options) type {
                 .black = &perspective.accs.getPtrConst(ntm).vec,
             });
 
-            const ob = if (obn == 1) 0 else if (obn == 8) blk: {
+            const ob = if (obn == 1) 0 else blk: {
                 const n = position.bothOcc().count();
                 const m = std.math.mulWide(u8, 63 - n, 32 - n);
                 break :blk @min(m / 225, 7);
-            } else unreachable;
+            };
             const wgts: std.EnumArray(types.Color, *const [l0s / 2]i16) = .init(.{
                 .white = self.l1w[ob][l0s / 2 * 0 ..][0 .. l0s / 2],
                 .black = self.l1w[ob][l0s / 2 * 1 ..][0 .. l0s / 2],
