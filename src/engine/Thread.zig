622b890610fb527e269706110c2c9476b22729c1
oilsoundsfunny <anbinhthuongnhatnam@gmail.com>
2026-02-25 23:19:02 +0700

    introduce history pruning + (inted) tune
    ```
    Elo   | 16.91 +- 8.41 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 2.90 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 3248 W: 1070 L: 912 D: 1266
    Penta | [80, 354, 650, 408, 132]
    ```
    https://oilsoundsfunny.pythonanywhere.com/test/14/
    
    ```
    Elo   | 3.77 +- 8.30 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 0.39 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 2950 W: 833 L: 801 D: 1316
    Penta | [74, 336, 620, 374, 71]
    ```
    https://oilsoundsfunny.pythonanywhere.com/test/15/
    
    todo: fix scaling
    bench: 6064936

--- src/engine/Thread.zig
+++ src/engine/Thread.zig
@@ -971,6 +971,21 @@ fn ab(
         const lmr_d = @max(d * 1024 - base_lmr, 0);
 
         if (!is_root and best.score > evaluation.score.lose) {
+            // history pruning
+            const hp_lim, const hp0, const hp1 = if (is_quiet) .{
+                params.values.quiet_hist_pruning_lim,
+                params.values.quiet_hist_pruning0,
+                params.values.quiet_hist_pruning1,
+            } else .{
+                params.values.noisy_hist_pruning_lim,
+                params.values.noisy_hist_pruning0,
+                params.values.noisy_hist_pruning1,
+            };
+            if (lmr_d <= hp_lim and sm.score < hp0 + hp1 * d) {
+                mp.skipQuiets();
+                continue :move_loop;
+            }
+
             // futility pruning
             // 10.0+0.1: 34.28 +- 12.73
             const fp_d = @divTrunc(lmr_d, 1024);
