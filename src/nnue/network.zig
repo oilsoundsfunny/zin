c5ee571f20cf68e6646060caa4fdd6d2961785f6
oilsoundsfunny <anbinhthuongnhatnam@gmail.com>
2026-03-12 13:06:34 +0700

    new arch (768hmx16->512pw)x2->1x8
    bench: 20506125

--- src/nnue/network.zig
+++ src/nnue/network.zig
@@ -15,21 +15,21 @@ const page_size = std.heap.pageSize();
 pub const Default = Network(.{
     .input_buckets = .{
         // zig fmt: off
-        0, 0, 1, 1,
-        2, 2, 2, 2,
-        3, 3, 3, 3,
-        3, 3, 3, 3,
-        4, 4, 5, 5,
-        4, 4, 5, 5,
-        4, 4, 5, 5,
-        4, 4, 5, 5,
+         0,  1,  2,  3,
+         4,  5,  6,  7,
+         8,  9, 10, 11,
+         8,  9, 10, 11,
+        12, 12, 13, 13,
+        12, 12, 13, 13,
+        14, 14, 15, 15,
+        14, 14, 15, 15,
         // zig fmt: on
     },
     .hl_size = 512,
     .output_buckets = 8,
     .qa = 255,
     .qb = 64,
-    .scale = 256,
+    .scale = 240,
 });
 
 pub const Options = struct {
