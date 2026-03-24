84e328ef32419f306e0188a68abb05ec88935ffe
oilsoundsfunny <anbinhthuongnhatnam@gmail.com>
2026-03-25 05:20:53 +0700

    new net
    bench: 8918794 nodes 830761 nps

--- build.zig
+++ build.zig
@@ -126,7 +126,7 @@ pub fn build(bld: *std.Build) !void {
     const network: std.Build.LazyPath = if (evalfile) |path|
         .{ .cwd_relative = path }
     else
-        bld.dependency("networks", .{}).path("1024hl-16b-8ob-220326.nnue");
+        bld.dependency("networks", .{}).path("1024hl-16b-8ob-250326.nnue");
 
     for (Modules.values) |m| {
         const deps = Modules.dependencies.get(m);
