117e85716558e7eda7c4ac937cf6a0b3176763ae
oilsoundsfunny <anbinhthuongnhatnam@gmail.com>
2026-06-09 11:31:01 +0700

    erm
    bench: 4127025

--- src/bitboard/magic.zig
+++ src/bitboard/magic.zig
@@ -7,128 +7,128 @@ const Magic = struct {
     pad: usize,
     ptr: [*]const types.Square.Set,
     magic: types.Square.Set.Int,
-    nmask: types.Square.Set,
+    mask: types.Square.Set,
 };
 
 const b_magic = std.EnumArray(types.Square, types.Square.Set.Int).init(.{
     // zig fmt: off
-    .a1 = 0xa7020080601803d8, .b1 = 0x13802040400801f1,
-    .c1 = 0x0a0080181001f60c, .d1 = 0x1840802004238008,
-    .e1 = 0xc03fe00100000000, .f1 = 0x24c00bffff400000,
-    .g1 = 0x0808101f40007f04, .h1 = 0x100808201ec00080,
-    .a2 = 0xffa2feffbfefb7ff, .b2 = 0x083e3ee040080801,
-    .c2 = 0xc0800080181001f8, .d2 = 0x0440007fe0031000,
-    .e2 = 0x2010007ffc000000, .f2 = 0x1079ffe000ff8000,
-    .g2 = 0x3c0708101f400080, .h2 = 0x080614080fa00040,
-    .a3 = 0x7ffe7fff817fcff9, .b3 = 0x7ffebfffa01027fd,
-    .c3 = 0x53018080c00f4001, .d3 = 0x407e0001000ffb8a,
-    .e3 = 0x201fe000fff80010, .f3 = 0xffdfefffde39ffef,
-    .g3 = 0xcc8808000fbf8002, .h3 = 0x7ff7fbfff8203fff,
-    .a4 = 0x8800013e8300c030, .b4 = 0x0420009701806018,
-    .c4 = 0x7ffeff7f7f01f7fd, .d4 = 0x8700303010c0c006,
-    .e4 = 0xc800181810606000, .f4 = 0x20002038001c8010,
-    .g4 = 0x087ff038000fc001, .h4 = 0x00080c0c00083007,
-    .a5 = 0x00000080fc82c040, .b5 = 0x000000407e416020,
-    .c5 = 0x00600203f8008020, .d5 = 0xd003fefe04404080,
-    .e5 = 0xa00020c018003088, .f5 = 0x7fbffe700bffe800,
-    .g5 = 0x107ff00fe4000f90, .h5 = 0x7f8fffcff1d007f8,
-    .a6 = 0x0000004100f88080, .b6 = 0x00000020807c4040,
-    .c6 = 0x00000041018700c0, .d6 = 0x0010000080fc4080,
-    .e6 = 0x1000003c80180030, .f6 = 0xc10000df80280050,
-    .g6 = 0xffffffbfeff80fdc, .h6 = 0x000000101003f812,
-    .a7 = 0x0800001f40808200, .b7 = 0x084000101f3fd208,
-    .c7 = 0x080000000f808081, .d7 = 0x0004000008003f80,
-    .e7 = 0x08000001001fe040, .f7 = 0x72dd000040900a00,
-    .g7 = 0xfffffeffbfeff81d, .h7 = 0xcd8000200febf209,
-    .a8 = 0x100000101ec10082, .b8 = 0x7fbaffffefe0c02f,
-    .c8 = 0x7f83fffffff07f7f, .d8 = 0xfff1fffffff7ffc1,
-    .e8 = 0x0878040000ffe01f, .f8 = 0x945e388000801012,
-    .g8 = 0x0840800080200fda, .h8 = 0x100000c05f582008,
+    .a1 = 0x007fbfbfbfbfbfff, .b1 = 0x0000a060401007fc,
+    .c1 = 0x0001004008020000, .d1 = 0x0000806004000000,
+    .e1 = 0x0000100400000000, .f1 = 0x000021c100b20000,
+    .g1 = 0x0000040041008000, .h1 = 0x00000fb0203fff80,
+    .a2 = 0x0000040100401004, .b2 = 0x0000020080200802,
+    .c2 = 0x0000004010202000, .d2 = 0x0000008060040000,
+    .e2 = 0x0000004402000000, .f2 = 0x0000000801008000,
+    .g2 = 0x000007efe0bfff80, .h2 = 0x0000000820820020,
+    .a3 = 0x0000400080808080, .b3 = 0x00021f0100400808,
+    .c3 = 0x00018000c06f3fff, .d3 = 0x0000258200801000,
+    .e3 = 0x0000240080840000, .f3 = 0x000018000c03fff8,
+    .g3 = 0x00000a5840208020, .h3 = 0x0000020008208020,
+    .a4 = 0x0000804000810100, .b4 = 0x0001011900802008,
+    .c4 = 0x0000804000810100, .d4 = 0x000100403c0403ff,
+    .e4 = 0x00078402a8802000, .f4 = 0x0000101000804400,
+    .g4 = 0x0000080800104100, .h4 = 0x00004004c0082008,
+    .a5 = 0x0001010120008020, .b5 = 0x000080809a004010,
+    .c5 = 0x0007fefe08810010, .d5 = 0x0003ff0f833fc080,
+    .e5 = 0x007fe08019003042, .f5 = 0x003fffefea003000,
+    .g5 = 0x0000101010002080, .h5 = 0x0000802005080804,
+    .a6 = 0x0000808080a80040, .b6 = 0x0000104100200040,
+    .c6 = 0x0003ffdf7f833fc0, .d6 = 0x0000008840450020,
+    .e6 = 0x00007ffc80180030, .f6 = 0x007fffdd80140028,
+    .g6 = 0x00020080200a0004, .h6 = 0x0000101010100020,
+    .a7 = 0x0007ffdfc1805000, .b7 = 0x0003ffefe0c02200,
+    .c7 = 0x0000000820806000, .d7 = 0x0000000008403000,
+    .e7 = 0x0000000100202000, .f7 = 0x0000004040802000,
+    .g7 = 0x0004010040100400, .h7 = 0x00006020601803f4,
+    .a8 = 0x0003ffdfdfc28048, .b8 = 0x0000000820820020,
+    .c8 = 0x0000000008208060, .d8 = 0x0000000000808020,
+    .e8 = 0x0000000001002020, .f8 = 0x0000000401002008,
+    .g8 = 0x0000004040404040, .h8 = 0x007fff9fdf7ff813,
     // zig fmt: on
 });
 
 const b_offset = std.EnumArray(types.Square, u32).init(.{
     // zig fmt: off
-    .a1 = 60984, .b1 = 66046, .c1 = 32910, .d1 = 16369,
-    .e1 = 42115, .f1 =   835, .g1 = 18910, .h1 = 25911,
-    .a2 = 63301, .b2 = 16063, .c2 = 17481, .d2 = 59361,
-    .e2 = 18735, .f2 = 61249, .g2 = 68938, .h2 = 61791,
-    .a3 = 21893, .b3 = 62068, .c3 = 19829, .d3 = 26091,
-    .e3 = 15815, .f3 = 16419, .g3 = 59777, .h3 = 16288,
-    .a4 = 33235, .b4 = 15459, .c4 = 15863, .d4 = 75555,
-    .e4 = 79445, .f4 = 15917, .g4 =  8512, .h4 = 73069,
-    .a5 = 16078, .b5 = 19168, .c5 = 11056, .d5 = 62544,
-    .e5 = 80477, .f5 = 75049, .g5 = 32947, .h5 = 59172,
-    .a6 = 55845, .b6 = 61806, .c6 = 73601, .d6 = 15546,
-    .e6 = 45243, .f6 = 20333, .g6 = 33402, .h6 = 25917,
-    .a7 = 32875, .b7 =  4639, .c7 = 17077, .d7 = 62324,
-    .e7 = 18159, .f7 = 61436, .g7 = 57073, .h7 = 61025,
-    .a8 = 81259, .b8 = 64083, .c8 = 56114, .d8 = 57058,
-    .e8 = 58912, .f8 = 22194, .g8 = 70880, .h8 = 11140,
+    .a1 =  5378, .b1 =  4093, .c1 =  4314, .d1 =  6587,
+    .e1 =  6491, .f1 =  6330, .g1 =  5609, .h1 = 22236,
+    .a2 =  6106, .b2 =  5625, .c2 = 16785, .d2 = 16817,
+    .e2 =  6842, .f2 =  7003, .g2 =  4197, .h2 =  7356,
+    .a3 =  4602, .b3 =  4538, .c3 = 29531, .d3 = 45393,
+    .e3 = 12420, .f3 = 15763, .g3 =  5050, .h3 =  4346,
+    .a4 =  6074, .b4 =  7866, .c4 = 32139, .d4 = 57673,
+    .e4 = 55365, .f4 = 15818, .g4 =  5562, .h4 =  6390,
+    .a5 =  7930, .b5 = 13329, .c5 =  7170, .d5 = 27267,
+    .e5 = 53787, .f5 =  5097, .g5 =  6643, .h5 =  6138,
+    .a6 =  7418, .b6 =  7898, .c6 = 42012, .d6 = 57350,
+    .e6 = 22813, .f6 = 56693, .g6 =  5818, .h6 =  7098,
+    .a7 =  4451, .b7 =  4709, .c7 =  4794, .d7 = 13364,
+    .e7 =  4570, .f7 =  4282, .g7 = 14964, .h7 =  4026,
+    .a8 =  4826, .b8 =  7354, .c8 =  4848, .d8 = 15946,
+    .e8 = 14932, .f8 = 16588, .g8 =  6905, .h8 = 16076,
     // zig fmt: on
 });
 
 const r_magic = std.EnumArray(types.Square, types.Square.Set.Int).init(.{
     // zig fmt: off
-    .a1 = 0x80280013ff84ffff, .b1 = 0x5ffbfefdfef67fff,
-    .c1 = 0xffeffaffeffdffff, .d1 = 0x003000900300008a,
-    .e1 = 0x0050028010500023, .f1 = 0x0020012120a00020,
-    .g1 = 0x0030006000c00030, .h1 = 0x0058005806b00002,
-    .a2 = 0x7fbff7fbfbeafffc, .b2 = 0x0000140081050002,
-    .c2 = 0x0000180043800048, .d2 = 0x7fffe800021fffb8,
-    .e2 = 0xffffcffe7fcfffaf, .f2 = 0x00001800c0180060,
-    .g2 = 0x4f8018005fd00018, .h2 = 0x0000180030620018,
-    .a3 = 0x00300018010c0003, .b3 = 0x0003000c0085ffff,
-    .c3 = 0xfffdfff7fbfefff7, .d3 = 0x7fc1ffdffc001fff,
-    .e3 = 0xfffeffdffdffdfff, .f3 = 0x7c108007befff81f,
-    .g3 = 0x20408007bfe00810, .h3 = 0x0400800558604100,
-    .a4 = 0x0040200010080008, .b4 = 0x0010020008040004,
-    .c4 = 0xfffdfefff7fbfff7, .d4 = 0xfebf7dfff8fefff9,
-    .e4 = 0xc00000ffe001ffe0, .f4 = 0x4af01f00078007c3,
-    .g4 = 0xbffbfafffb683f7f, .h4 = 0x0807f67ffa102040,
-    .a5 = 0x200008e800300030, .b5 = 0x0000008780180018,
-    .c5 = 0x0000010300180018, .d5 = 0x4000008180180018,
-    .e5 = 0x008080310005fffa, .f5 = 0x4000188100060006,
-    .g5 = 0xffffff7fffbfbfff, .h5 = 0x0000802000200040,
-    .a6 = 0x20000202ec002800, .b6 = 0xfffff9ff7cfff3ff,
-    .c6 = 0x000000404b801800, .d6 = 0x2000002fe03fd000,
-    .e6 = 0xffffff6ffe7fcffd, .f6 = 0xbff7efffbfc00fff,
-    .g6 = 0x000000100800a804, .h6 = 0x6054000a58005805,
-    .a7 = 0x0829000101150028, .b7 = 0x00000085008a0014,
-    .c7 = 0x8000002b00408028, .d7 = 0x4000002040790028,
-    .e7 = 0x7800002010288028, .f7 = 0x0000001800e08018,
-    .g7 = 0xa3a80003f3a40048, .h7 = 0x2003d80000500028,
-    .a8 = 0xfffff37eefefdfbe, .b8 = 0x40000280090013c1,
-    .c8 = 0xbf7ffeffbffaf71f, .d8 = 0xfffdffff777b7d6e,
-    .e8 = 0x48300007e8080c02, .f8 = 0xafe0000fff780402,
-    .g8 = 0xee73fffbffbb77fe, .h8 = 0x0002000308482882,
+    .a1 = 0x00280077ffebfffe, .b1 = 0x2004010201097fff,
+    .c1 = 0x0010020010053fff, .d1 = 0x0040040008004002,
+    .e1 = 0x7fd00441ffffd003, .f1 = 0x4020008887dffffe,
+    .g1 = 0x004000888847ffff, .h1 = 0x006800fbff75fffd,
+    .a2 = 0x000028010113ffff, .b2 = 0x0020040201fcffff,
+    .c2 = 0x007fe80042ffffe8, .d2 = 0x00001800217fffe8,
+    .e2 = 0x00001800073fffe8, .f2 = 0x00001800e05fffe8,
+    .g2 = 0x00001800602fffe8, .h2 = 0x000030002fffffa0,
+    .a3 = 0x00300018010bffff, .b3 = 0x0003000c0085fffb,
+    .c3 = 0x0004000802010008, .d3 = 0x0004002020020004,
+    .e3 = 0x0001002002002001, .f3 = 0x0001001000801040,
+    .g3 = 0x0000004040008001, .h3 = 0x0000006800cdfff4,
+    .a4 = 0x0040200010080010, .b4 = 0x0000080010040010,
+    .c4 = 0x0004010008020008, .d4 = 0x0000040020200200,
+    .e4 = 0x0002008010100100, .f4 = 0x0000008020010020,
+    .g4 = 0x0000008020200040, .h4 = 0x0000820020004020,
+    .a5 = 0x00fffd1800300030, .b5 = 0x007fff7fbfd40020,
+    .c5 = 0x003fffbd00180018, .d5 = 0x001fffde80180018,
+    .e5 = 0x000fffe0bfe80018, .f5 = 0x0001000080202001,
+    .g5 = 0x0003fffbff980180, .h5 = 0x0001fffdff9000e0,
+    .a6 = 0x00fffefeebffd800, .b6 = 0x007ffff7ffc01400,
+    .c6 = 0x003fffbfe4ffe800, .d6 = 0x001ffff01fc03000,
+    .e6 = 0x000fffe7f8bfe800, .f6 = 0x0007ffdfdf3ff808,
+    .g6 = 0x0003fff85fffa804, .h6 = 0x0001fffd75ffa802,
+    .a7 = 0x00ffffd7ffebffd8, .b7 = 0x007fff75ff7fbfd8,
+    .c7 = 0x003fff863fbf7fd8, .d7 = 0x001fffbfdfd7ffd8,
+    .e7 = 0x000ffff810280028, .f7 = 0x0007ffd7f7feffd8,
+    .g7 = 0x0003fffc0c480048, .h7 = 0x0001ffffafd7ffd8,
+    .a8 = 0x00ffffe4ffdfa3ba, .b8 = 0x007fffef7ff3d3da,
+    .c8 = 0x003fffbfdfeff7fa, .d8 = 0x001fffeff7fbfc22,
+    .e8 = 0x0000020408001001, .f8 = 0x0007fffeffff77fd,
+    .g8 = 0x0003ffffbf7dfeec, .h8 = 0x0001ffff9dffa333,
     // zig fmt: on
 });
 
 const r_offset = std.EnumArray(types.Square, u32).init(.{
     // zig fmt: off
-    .a1 = 10890, .b1 = 50579, .c1 = 62020, .d1 = 67322,
-    .e1 = 80251, .f1 = 58503, .g1 = 51175, .h1 = 83130,
-    .a2 = 50430, .b2 = 21613, .c2 = 72625, .d2 = 80755,
-    .e2 = 69753, .f2 = 26973, .g2 = 84972, .h2 = 31958,
-    .a3 = 69272, .b3 = 48372, .c3 = 65477, .d3 = 43972,
-    .e3 = 57154, .f3 = 53521, .g3 = 30534, .h3 = 16548,
-    .a4 = 46407, .b4 = 11841, .c4 = 21112, .d4 = 44214,
-    .e4 = 57925, .f4 = 29574, .g4 = 17309, .h4 = 40143,
-    .a5 = 64659, .b5 = 70469, .c5 = 62917, .d5 = 60997,
-    .e5 = 18554, .f5 = 14385, .g5 =     0, .h5 = 38091,
-    .a6 = 25122, .b6 = 60083, .c6 = 72209, .d6 = 67875,
-    .e6 = 56290, .f6 = 43807, .g6 = 73365, .h6 = 76398,
-    .a7 = 20024, .b7 =  9513, .c7 = 24324, .d7 = 22996,
-    .e7 = 23213, .f7 = 56002, .g7 = 22809, .h7 = 44545,
-    .a8 = 36072, .b8 =  4750, .c8 =  6014, .d8 = 36054,
-    .e8 = 78538, .f8 = 28745, .g8 =  8555, .h8 =  1009,
+    .a1 = 26304, .b1 = 35520, .c1 = 38592, .d1 =  8026,
+    .e1 = 22196, .f1 = 80870, .g1 = 76747, .h1 = 30400,
+    .a2 = 11115, .b2 = 18205, .c2 = 53577, .d2 = 62724,
+    .e2 = 34282, .f2 = 29196, .g2 = 23806, .h2 = 49481,
+    .a3 =  2410, .b3 = 36498, .c3 = 24478, .d3 = 10074,
+    .e3 = 79315, .f3 = 51779, .g3 = 13586, .h3 = 19323,
+    .a4 = 70612, .b4 = 83652, .c4 = 63110, .d4 = 34496,
+    .e4 = 84966, .f4 = 54341, .g4 = 60421, .h4 = 86402,
+    .a5 = 50245, .b5 = 76622, .c5 = 84676, .d5 = 78757,
+    .e5 = 37346, .f5 =   370, .g5 = 42182, .h5 = 45385,
+    .a6 = 61659, .b6 = 12790, .c6 = 16762, .d6 =     0,
+    .e6 = 38380, .f6 = 11098, .g6 = 21803, .h6 = 39189,
+    .a7 = 58628, .b7 = 44116, .c7 = 78357, .d7 = 44481,
+    .e7 = 64134, .f7 = 41759, .g7 =  1394, .h7 = 40910,
+    .a8 = 66516, .b8 =  3897, .c8 =  3930, .d8 = 72934,
+    .e8 = 72662, .f8 = 56325, .g8 = 66501, .h8 = 14826,
     // zig fmt: on
 });
 
 var b_tbl: std.EnumArray(types.Square, Magic) align(std.atomic.cache_line) = .initUndefined();
 var r_tbl: std.EnumArray(types.Square, Magic) align(std.atomic.cache_line) = .initUndefined();
-var atk: [87988]types.Square.Set align(std.atomic.cache_line) = undefined;
+var atk: [88772]types.Square.Set align(std.atomic.cache_line) = undefined;
 
 fn bGenAtk(s: types.Square, b: types.Square.Set) types.Square.Set {
     return utils.genAtk(.bishop, s, b);
@@ -140,14 +140,14 @@ fn rGenAtk(s: types.Square, b: types.Square.Set) types.Square.Set {
 
 fn bGenIdx(s: types.Square, b: types.Square.Set) types.Square.Set.Int {
     const magic = b_tbl.getPtrConst(s).magic;
-    const nmask = b_tbl.getPtrConst(s).nmask;
-    return b.bwo(nmask).int() *% magic >> (64 - 9);
+    const mask = b_tbl.getPtrConst(s).mask;
+    return b.bwa(mask).int() *% magic >> (64 - 9);
 }
 
 fn rGenIdx(s: types.Square, b: types.Square.Set) types.Square.Set.Int {
     const magic = r_tbl.getPtrConst(s).magic;
-    const nmask = r_tbl.getPtrConst(s).nmask;
-    return b.bwo(nmask).int() *% magic >> (64 - 12);
+    const mask = r_tbl.getPtrConst(s).mask;
+    return b.bwa(mask).int() *% magic >> (64 - 12);
 }
 
 fn bAtkInit() !void {
@@ -164,15 +164,14 @@ fn bAtkInit() !void {
 
         const offset = b_offset.getPtrConst(s).*;
         const magic = b_magic.getPtrConst(s).*;
-        const nmask = mask.flip();
 
         const n = std.math.shl(usize, 1, mask.count());
         const p = atk[offset..].ptr;
         b_tbl.set(s, .{
             .ptr = p,
             .magic = magic,
-            .nmask = nmask,
-            .pad = 0xaaaaaaaaaaaaaaaa,
+            .mask = mask,
+            .pad = undefined,
         });
 
         var b = types.Square.Set.none;
@@ -181,8 +180,10 @@ fn bAtkInit() !void {
             const i = bGenIdx(s, b);
 
             p[i] = a;
+            // zig fmt: off
             b = .fromInt(b.int() -% mask.int());
-            b.popOther(nmask);
+            b = .fromInt(b.int() &  mask.int());
+            // zig fmt: on
         }
     }
 }
@@ -201,15 +202,14 @@ fn rAtkInit() !void {
 
         const offset = r_offset.getPtrConst(s).*;
         const magic = r_magic.getPtrConst(s).*;
-        const nmask = mask.flip();
 
         const n = std.math.shl(usize, 1, mask.count());
         const p = atk[offset..].ptr;
         r_tbl.set(s, .{
             .ptr = p,
             .magic = magic,
-            .nmask = nmask,
-            .pad = 0xaaaaaaaaaaaaaaaa,
+            .mask = mask,
+            .pad = undefined,
         });
 
         var b = types.Square.Set.none;
@@ -218,8 +218,10 @@ fn rAtkInit() !void {
             const i = rGenIdx(s, b);
 
             p[i] = a;
+            // zig fmt: off
             b = .fromInt(b.int() -% mask.int());
-            b.popOther(nmask);
+            b = .fromInt(b.int() &  mask.int());
+            // zig fmt: on
         }
     }
 }
